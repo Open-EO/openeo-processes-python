b5ba7956c6df6bf7bab745632c1a78473c0d7ded
Luca Foresta <luca.foresta@eodc.eu>
2020-12-14 11:25:05 +0100

    integrate unittests and pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,77 +4,51 @@ import pytest
 import xarray as xr
 
 
-@pytest.fixture()
-def np_array():
-    return get_numpy()
+@pytest.fixture(scope="class")
+def test_data(request):
+    class TestDataDriver:
+        def __init__(self):
+            self.steps = {'y': 5, 'x': 3}
+            self.coords_4d = {
+                's': ['B08', 'B04', 'B02'],
+                't': [datetime(2019, 12, 1), datetime(2019, 12, 5)],
+                'y': np.arange(55.3, 55.3 + self.steps['y']),
+                'x': np.arange(118.9, 118.9 + self.steps['x'])
+            }
+            self.coords_3d = {
+                't': [datetime(2019, 12, 1), datetime(2019, 12, 5)],
+                'y': np.arange(55.3, 55.3 + self.steps['y']),
+                'x': np.arange(118.9, 118.9 + self.steps['x'])
+            }
+            self._get_numpy()
+            self._get_xarray()
 
+        def _get_numpy(self):
+            """
+            Returns a fixed numpy array with 4 dimensions.
+            """
 
-@pytest.fixture()
-def xr_array():
-    """
-    Returns a fixed xarray DataArray array with 4 labelled dimensions
-    with coordinates.
-    """
+            data = np.ones((3, 2, self.steps['y'], self.steps['x']))
+            data[0, :] *= 8  # identify band 8 by its value
+            data[1, :] *= 4  # identify band 4 by its value
+            data[2, :] *= 2  # identify band 2 by its value
 
-    data = get_numpy()
-    coords = get_coords()
-    xr_data = xr.DataArray(data=data, dims=coords.keys(), coords=coords)
+            data[:, 1, :] *= 10  # second t-step of each band multiplied by 10
 
-    return xr_data
+            self.np_data_4d = data
+            self.np_data_3d = data[0, :]
 
+        def _get_xarray(self):
+            """
+            Returns a fixed xarray DataArray array with 3 labelled dimensions
+            with coordinates.
+            """
 
-@pytest.fixture()
-def xr_array_B08():
-    """
-    Returns a fixed xarray DataArray array with 3 labelled dimensions
-    with coordinates.
-    """
+            self.xr_data_4d = xr.DataArray(data=self.np_data_4d,
+                                           dims=self.coords_4d.keys(),
+                                           coords=self.coords_4d)
+            self.xr_data_3d = xr.DataArray(data=self.np_data_3d,
+                                           dims=self.coords_3d.keys(),
+                                           coords=self.coords_3d)
 
-    data = get_numpy()
-    data = data[0, :]
-    coords = get_coords()
-    _ = coords.pop('s')
-
-    xr_data = xr.DataArray(data=data, dims=coords.keys(), coords=coords)
-
-    return xr_data
-
-
-def get_numpy():
-    """
-    Returns a fixed numpy array with 4 dimensions.
-    """
-
-    steps = get_steps()
-    size_x = steps['x']
-    size_y = steps['y']
-
-    data = np.ones((3, 2, size_y, size_x))
-    data[0, :] *= 8  # identify band 8 by its value
-    data[1, :] *= 4  # identify band 4 by its value
-    data[2, :] *= 2  # identify band 2 by its value
-
-    data[:, 1, :] *= 10  # second time step of each band multiplied by 10
-
-    return data
-
-
-def get_coords():
-
-    steps = get_steps()
-
-    coords = {
-        's': ['B08', 'B04', 'B02'],
-        't': [datetime(2019, 12, 1), datetime(2019, 12, 5)],
-        'y': np.arange(55.3, 55.3 + steps['y']),
-        'x': np.arange(118.9, 118.9 + steps['x'])
-    }
-    return coords
-
-
-def get_steps():
-    steps = {
-        'y': 5,
-        'x': 3
-    }
-    return steps
+    request.cls.test_data = TestDataDriver()
--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -5,148 +5,150 @@ https://openeo.org/documentation/1.0/processes.html
 
 import numpy as np
 import openeo_processes.arrays as oeop
+import pytest
 import xarray as xr
+from unittest import TestCase
 
 
-def test_array_contains():
-    """ Tests `array_contains` function. """
-    assert oeop.array_contains([1, 2, 3], value=2)
-    assert not oeop.array_contains(["A", "B", "C"], value="b")
-    assert not oeop.array_contains([1, 2, 3], value="2")
-    assert oeop.array_contains([1, 2, 3], value=2)
-    assert oeop.array_contains([1, 2, np.nan], value=np.nan)
-    assert oeop.array_contains([[1, 2], [3, 4]], value=[1, 2])
-    assert not oeop.array_contains([[1, 2], [3, 4]], value=2)
-    assert oeop.array_contains([{"a": "b"}, {"c": "d"}], value={"a": "b"})
+@pytest.mark.usefixtures("test_data")
+class ArrayTester(TestCase):
+    """ Tests all array functions. """
 
+    def test_array_contains(self):
+        """ Tests `array_contains` function. """
+        assert oeop.array_contains([1, 2, 3], value=2)
+        assert not oeop.array_contains(["A", "B", "C"], value="b")
+        assert not oeop.array_contains([1, 2, 3], value="2")
+        assert oeop.array_contains([1, 2, 3], value=2)
+        assert oeop.array_contains([1, 2, np.nan], value=np.nan)
+        assert oeop.array_contains([[1, 2], [3, 4]], value=[1, 2])
+        assert not oeop.array_contains([[1, 2], [3, 4]], value=2)
+        assert oeop.array_contains([{"a": "b"}, {"c": "d"}], value={"a": "b"})
 
-def test_array_element(xr_array, xr_array_B08):
-    """ Tests `array_element` function. """
-    # numpy tests
-    assert oeop.array_element([9, 8], label="B", labels=np.array(["A", "B"])) == 8
-    assert oeop.array_element([9, 8, 7, 6, 5], index=2) == 7
-    assert oeop.array_element(["A", "B", "C"], index=0) == "A"
-    assert np.isnan(oeop.array_element([], index=0, return_nodata=True))
+    def test_array_element(self):
+        """ Tests `array_element` function. """
+        # numpy tests
+        assert oeop.array_element([9, 8], label="B", labels=np.array(["A", "B"])) == 8
+        assert oeop.array_element([9, 8, 7, 6, 5], index=2) == 7
+        assert oeop.array_element(["A", "B", "C"], index=0) == "A"
+        assert np.isnan(oeop.array_element([], index=0, return_nodata=True))
 
-    # multi-dim
-    test_array = np.empty((3, 2, 2))
-    test_array[0, :, :] = np.array([[1, 2], [3, 4]])
-    test_array[1, :, :] = np.array([[1, 2], [3, 4]]) * 20
-    test_array[2, :, :] = np.array([[1, 2], [3, 4]]) * 500
-    array_i2_d0 = np.array([
-                            [500, 1000],
-                            [1500, 2000]
-                            ])
-    array_i0_d1 = np.array([
-                            [1, 2],
-                            [20, 40],
-                            [500, 1000]
-                            ])
-    assert np.isclose(oeop.array_element(test_array, index=2, dimension=0), array_i2_d0, equal_nan=True).all()
-    assert np.isclose(oeop.array_element(test_array, index=0, dimension=1), array_i0_d1, equal_nan=True).all()
+        # multi-dim
+        test_array = np.empty((3, 2, 2))
+        test_array[0, :, :] = np.array([[1, 2], [3, 4]])
+        test_array[1, :, :] = np.array([[1, 2], [3, 4]]) * 20
+        test_array[2, :, :] = np.array([[1, 2], [3, 4]]) * 500
+        array_i2_d0 = np.array([
+                                [500, 1000],
+                                [1500, 2000]
+                                ])
+        array_i0_d1 = np.array([
+                                [1, 2],
+                                [20, 40],
+                                [500, 1000]
+                                ])
+        assert np.isclose(oeop.array_element(test_array, index=2, dimension=0), array_i2_d0, equal_nan=True).all()
+        assert np.isclose(oeop.array_element(test_array, index=0, dimension=1), array_i0_d1, equal_nan=True).all()
 
-    # xarray tests
-    xr.testing.assert_equal(
-        oeop.array_element(xr_array, dimension='s', label="B08"),
-        xr_array_B08)
-    xr.testing.assert_equal(
-        oeop.array_element(xr_array, dimension='s', index=0),
-        xr_array_B08)
-    # Assert raised errors?
-    # ArrayElementNotAvailable
-    # oeop.array_element(xr_array, dimension='s', label="B09")
-    # oeop.array_element(xr_array, dimension='s', index=4)
+        # xarray tests
+        xr.testing.assert_equal(
+            oeop.array_element(self.test_data.xr_data_4d, dimension='s', label="B08"),
+            self.test_data.xr_data_3d)
+        xr.testing.assert_equal(
+            oeop.array_element(self.test_data.xr_data_4d, dimension='s', index=0),
+            self.test_data.xr_data_3d)
+        # Assert raised errors?
+        # ArrayElementNotAvailable
+        # oeop.array_element(self.xr_data_4d, dimension='s', label="B09")
+        # oeop.array_element(self.xr_data_4d, dimension='s', index=4)
 
-def test_count():
-    """ Tests `count` function. """
-    assert oeop.count([]) == 0
-    assert oeop.count([1, 0, 3, 2]) == 4
-    assert oeop.count(["ABC", np.nan]) == 1
-    assert oeop.count([False, np.nan], condition=True) == 2
-    assert oeop.count([0, 1, 2, 3, 4, 5, np.nan], condition=oeop.gt, context={'y': 2})
+    def test_count(self):
+        """ Tests `count` function. """
+        assert oeop.count([]) == 0
+        assert oeop.count([1, 0, 3, 2]) == 4
+        assert oeop.count(["ABC", np.nan]) == 1
+        assert oeop.count([False, np.nan], condition=True) == 2
+        assert oeop.count([0, 1, 2, 3, 4, 5, np.nan], condition=oeop.gt, context={'y': 2})
 
-#TODO: add test
-def test_array_apply():
-    """ Tests `array_apply` function. """
-    pass
+    # TODO: add test
+    def test_array_apply(self):
+        """ Tests `array_apply` function. """
+        pass
 
-# TODO: add test
-def test_array_filter():
-    """ Tests `array_filter` function. """
-    pass
+    # TODO: add test
+    def test_array_filter(self):
+        """ Tests `array_filter` function. """
+        pass
 
-# TODO: add test
-def test_array_find():
-    """ Tests `array_find` function. """
-    pass
+    # TODO: add test
+    def test_array_find(self):
+        """ Tests `array_find` function. """
+        pass
 
-# TODO: add test
-def test_array_labels():
-    """ Tests `array_labels` function. """
-    pass
+    # TODO: add test
+    def test_array_labels(self):
+        """ Tests `array_labels` function. """
+        pass
 
-def test_first():
-    """ Tests `first` function. """
-    assert oeop.first([1, 0, 3, 2]) == 1
-    assert oeop.first([np.nan, "A", "B"]) == "A"
-    assert np.isnan(oeop.first([np.nan, 2, 3], ignore_nodata=False))
-    assert np.isnan(oeop.first([]))
+    def test_first(self):
+        """ Tests `first` function. """
+        assert oeop.first([1, 0, 3, 2]) == 1
+        assert oeop.first([np.nan, "A", "B"]) == "A"
+        assert np.isnan(oeop.first([np.nan, 2, 3], ignore_nodata=False))
+        assert np.isnan(oeop.first([]))
 
-    # 2D test
-    test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 2]], [[1, 2], [1, np.nan]]])
-    first_elem_ref = np.array([[[3., 2.], [1., 2.]]])
-    first_elem = oeop.first(test_arr)
-    assert np.isclose(first_elem, first_elem_ref, equal_nan=True).all()
-    test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 2]], [[1, 2], [1, np.nan]]])
-    first_elem_ref = np.array([[[np.nan, 2.], [1., 2.]]])
-    first_elem = oeop.first(test_arr, ignore_nodata=False)
-    assert np.isclose(first_elem, first_elem_ref, equal_nan=True).all()
+        # 2D test
+        test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 2]], [[1, 2], [1, np.nan]]])
+        first_elem_ref = np.array([[[3., 2.], [1., 2.]]])
+        first_elem = oeop.first(test_arr)
+        assert np.isclose(first_elem, first_elem_ref, equal_nan=True).all()
+        test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 2]], [[1, 2], [1, np.nan]]])
+        first_elem_ref = np.array([[[np.nan, 2.], [1., 2.]]])
+        first_elem = oeop.first(test_arr, ignore_nodata=False)
+        assert np.isclose(first_elem, first_elem_ref, equal_nan=True).all()
 
-def test_last():
-    """ Tests `last` function. """
-    assert oeop.last([1, 0, 3, 2]) == 2
-    assert oeop.last(["A", "B", np.nan]) == "B"
-    assert np.isnan(oeop.last([0, 1, np.nan], ignore_nodata=False))
-    assert np.isnan(oeop.last([]))
+    def test_last(self):
+        """ Tests `last` function. """
+        assert oeop.last([1, 0, 3, 2]) == 2
+        assert oeop.last(["A", "B", np.nan]) == "B"
+        assert np.isnan(oeop.last([0, 1, np.nan], ignore_nodata=False))
+        assert np.isnan(oeop.last([]))
 
-    # 2D test
-    test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 3]], [[1, 2], [1, np.nan]]])
-    last_elem_ref = np.array([[[1., 2.], [1., 3.]]])
-    last_elem = oeop.last(test_arr)
-    assert np.isclose(last_elem, last_elem_ref, equal_nan=True).all()
-    test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 2]], [[1, 2], [1, np.nan]]])
-    last_elem_ref = np.array([[[1., 2.], [1., np.nan]]])
-    last_elem = oeop.last(test_arr, ignore_nodata=False)
-    assert np.isclose(last_elem, last_elem_ref, equal_nan=True).all()
+        # 2D test
+        test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 3]], [[1, 2], [1, np.nan]]])
+        last_elem_ref = np.array([[[1., 2.], [1., 3.]]])
+        last_elem = oeop.last(test_arr)
+        assert np.isclose(last_elem, last_elem_ref, equal_nan=True).all()
+        test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 2]], [[1, 2], [1, np.nan]]])
+        last_elem_ref = np.array([[[1., 2.], [1., np.nan]]])
+        last_elem = oeop.last(test_arr, ignore_nodata=False)
+        assert np.isclose(last_elem, last_elem_ref, equal_nan=True).all()
 
-# def test_order():
-#     """ Tests `order` function. """
-#     .assertListEqual(oeop.order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9]).tolist(),
-#                          [1, 2, 8, 5, 0, 4, 7, 9, 10])
-#     .assertListEqual(oeop.order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], nodata=True).tolist(),
-#                          [1, 2, 8, 5, 0, 4, 7, 9, 10, 3, 6])
-#     .assertListEqual(oeop.order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], asc=False, nodata=True).tolist(),
-#                          [9, 10, 7, 4, 0, 5, 8, 2, 1, 3, 6])
-#     .assertListEqual(oeop.order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], asc=False, nodata=False).tolist(),
-#                          [6, 3, 9, 10, 7, 4, 0, 5, 8, 2, 1])
-# 
-# def test_rearrange():
-#     """ Tests `rearrange` function. """
-#     .assertListEqual(oeop.rearrange([5, 4, 3], [2, 1, 0]).tolist(), [3, 4, 5])
-#     .assertListEqual(oeop.rearrange([5, 4, 3, 2], [1, 3]).tolist(), [4, 2])
-#     .assertListEqual(oeop.rearrange([5, 4, 3, 2], [0, 2, 1, 3]).tolist(), [5, 3, 4, 2])
-# 
-# def test_sort():
-#     """ Tests `sort` function. """
-#     .assertListEqual(oeop.sort([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9]).tolist(),
-#                          [-1, 2, 3, 4, 6, 7, 8, 9, 9])
-#     assert np.isclose(oeop.sort([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], asc=False, nodata=True),
-#                       [9, 9, 8, 7, 6, 4, 3, 2, -1, np.nan, np.nan], equal_nan=True).all()
+    def test_order(self):
+        """ Tests `order` function. """
+        self.assertListEqual(oeop.order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9]).tolist(),
+                             [1, 2, 8, 5, 0, 4, 7, 9, 10])
+        self.assertListEqual(oeop.order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], nodata=True).tolist(),
+                             [1, 2, 8, 5, 0, 4, 7, 9, 10, 3, 6])
+        self.assertListEqual(oeop.order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], asc=False, nodata=True).tolist(),
+                             [9, 10, 7, 4, 0, 5, 8, 2, 1, 3, 6])
+        self.assertListEqual(oeop.order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], asc=False, nodata=False).tolist(),
+                             [6, 3, 9, 10, 7, 4, 0, 5, 8, 2, 1])
 
-# TODO: add test
-def test_mask():
-    """ Tests `mask` function. """
-    pass
+    def test_rearrange(self):
+        """ Tests `rearrange` function. """
+        self.assertListEqual(oeop.rearrange([5, 4, 3], [2, 1, 0]).tolist(), [3, 4, 5])
+        self.assertListEqual(oeop.rearrange([5, 4, 3, 2], [0, 2, 1, 3]).tolist(), [5, 3, 4, 2])
+        self.assertListEqual(oeop.rearrange([5, 4, 3, 2], [1, 3]).tolist(), [4, 2])
 
-# if __name__ == "__main__":
-#     unittest.main()
+    def test_sort(self):
+        """ Tests `sort` function. """
+        self.assertListEqual(oeop.sort([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9]).tolist(),
+                             [-1, 2, 3, 4, 6, 7, 8, 9, 9])
+        assert np.isclose(oeop.sort([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], asc=False, nodata=True),
+                          [9, 9, 8, 7, 6, 4, 3, 2, -1, np.nan, np.nan], equal_nan=True).all()
+
+    # TODO: add test
+    def test_mask(self):
+        """ Tests `mask` function. """
+        pass
